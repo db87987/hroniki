4bb76d6d06388b5f52d9c76afb389cf181327715
Viacheslav Bobrov <babrovka@gmail.com>
2013-08-28 17:25:11 +0400

    update recipe

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,25 +1,44 @@
-set :application, "set your application name here"
-set :repository,  "set your repository location here"
+require 'rvm/capistrano'
+require 'bundler/capistrano'
 
-# set :scm, :git # You can set :scm explicitly or Capistrano will make an intelligent guess based on known version control directory names
-# Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
+load 'deploy/assets'
 
-role :web, "your web-server here"                          # Your HTTP server, Apache/etc
-role :app, "your app-server here"                          # This may be the same as your `Web` server
-role :db,  "your primary db-server here", :primary => true # This is where Rails migrations will run
-role :db,  "your slave db-server here"
+server "5.178.80.26", :web, :app, :db, primary: true
 
-# if you want to clean up old releases on each deploy uncomment this:
-# after "deploy:restart", "deploy:cleanup"
+set :user, "user"
+set :application, "hroniki"
+set :deploy_to, "/home/user/projects/#{application}"
+set :deploy_via, :remote_cache
+set :use_sudo, false
 
-# if you're still using the script/reaper helper you will need
-# these http://github.com/rails/irs_process_scripts
+set :scm, "git"
+set :repository, "git@github.com:babrovka/hroniki.git"
+set :branch, "master"
 
-# If you are using Passenger mod_rails uncomment this:
-# namespace :deploy do
-#   task :start do ; end
-#   task :stop do ; end
-#   task :restart, :roles => :app, :except => { :no_release => true } do
-#     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
-#   end
-# end
+default_run_options[:pty] = true
+ssh_options[:forward_agent] = true
+
+
+task :copy_database_config do
+   db_config = "#{shared_path}/database.yml"
+   run "cp #{db_config} #{latest_release}/config/database.yml"
+end
+
+
+namespace(:thin) do
+  task :stop do
+    run "thin stop -C /etc/thin/hroniki.yml"
+   end
+  
+  task :start do
+    run "thin start -C /etc/thin/hroniki.yml"
+  end
+
+  task :restart do
+    run "thin restart -C /etc/thin/hroniki.yml"
+  end
+end
+
+
+before "deploy:assets:precompile", "copy_database_config"
+after "deploy", "deploy:cleanup"
