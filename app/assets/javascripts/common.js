118d95559dd3989b39f39277836f901184fdf004
Viacheslav Bobrov <babrovka@gmail.com>
2013-09-04 18:23:35 +0400

    hroniki index img hover

--- app/assets/javascripts/common.js
+++ app/assets/javascripts/common.js
@@ -2,4 +2,14 @@ $(document).ready(function() {
     $("#header_link").on('click', function () {
 		$(location).attr('href', '/');
 	});
+	
+	$(".hroniki_image_container").on("mouseover",function () {
+		$(this).find('.hroniki_hover_container').show();
+	});
+	
+
+	$(".hroniki_image_container").mouseleave(function () {
+		$('.hroniki_hover_container').hide();
+	});
+	
 });
