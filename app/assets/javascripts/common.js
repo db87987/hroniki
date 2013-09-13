68496c3e304986598e488e52d0e0b29ab9ebbde5
Viacheslav Bobrov <babrovka@gmail.com>
2013-09-13 16:02:18 +0400

    hroniki show

--- app/assets/javascripts/common.js
+++ app/assets/javascripts/common.js
@@ -12,4 +12,13 @@ $(document).ready(function() {
 		$('.hroniki_hover_container').hide();
 	});
 	
+	$(".hroniki_image_container").on("mouseover",function () {
+		$(this).find('.hroniki_hover_small_container').show();
+	});
+	
+
+	$(".hroniki_image_container").mouseleave(function () {
+		$('.hroniki_hover_small_container').hide();
+	});
+	
 });
