ff73706c23d9bffe9f354c697c70f518e16f457f
Viacheslav Bobrov <babrovka@gmail.com>
2013-09-16 15:28:51 +0400

    blog show image

--- app/models/article.rb
+++ app/models/article.rb
@@ -7,5 +7,5 @@ class Article < ActiveRecord::Base
   
   validates :date, :text, :title, presence: true
   
-  has_attached_file :image, :styles => { :side => "220x220#", :ico => "100x100#" }
+  has_attached_file :image, :styles => { :side => "220x220#", :ico => "100x100#", :show => "640x460>" }
 end
