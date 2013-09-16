e1e7518ce235348a1b21931307567f5bd3aa2404
Viacheslav Bobrov <babrovka@gmail.com>
2013-09-16 15:18:48 +0400

    blog vk comment

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -15,10 +15,17 @@ class ArticlesController < ApplicationController
   end
 
   def comment
+    api_secret = "GfqyAEOfSrOYwssRtbOk"
+    last_comment = params[:last_comment].encode Encoding::WINDOWS_1251
+    hash = Digest::MD5.hexdigest(api_secret+params[:date]+params[:num]+last_comment)
+    sign = params[:sign]
+    if hash == sign
       @article = Article.find(params[:id])
       @article.comments_qty = params[:num]
       @article.save
-      
+    else
+      nil
+    end
       render nothing: true
   end
 end
