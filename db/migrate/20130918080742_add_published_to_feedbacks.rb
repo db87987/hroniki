06934bc3fad46980137244f650a355cd41cf25cc
Viacheslav Bobrov <babrovka@gmail.com>
2013-09-18 12:45:54 +0400

    feedbacks

--- db/migrate/20130918080742_add_published_to_feedbacks.rb
+++ db/migrate/20130918080742_add_published_to_feedbacks.rb
@@ -1,6 +1,6 @@
 class AddPublishedToFeedbacks < ActiveRecord::Migration
   def change
-    remove_column :feedbacks, :published, :boolean
+    remove_column :feedbacks, :published
     add_column :feedbacks, :published, :boolean, :default => false
   end
 end
