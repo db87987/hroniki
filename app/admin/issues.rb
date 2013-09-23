bfe7099e7b95c19fe36ff1d5e44b748e601144c0
Viacheslav Bobrov <babrovka@gmail.com>
2013-09-23 17:53:32 +0400

    issue manipulaton

--- app/admin/issues.rb
+++ app/admin/issues.rb
@@ -4,21 +4,32 @@ ActiveAdmin.register Issue do
   config.batch_actions = false
   config.clear_sidebar_sections!
   
-   index do 
-     column :title
-     default_actions
-   end
+  index do
+    column :id
+    column :title
+    column :date
+    column :image do |column|
+      image_tag column.image.url(:ico)
+    end
+    default_actions
+  end
 
    form do |f|  
      f.inputs t('properties') do
        f.input :title
+       f.input :date
+       f.input :image, :as => :file, :hint => ( f.object.new_record? || !f.object.image ) ? nil : image_tag(f.object.image.url(:ico))
      end
      f.actions
    end
 
   show do
-    attributes_table do
+    attributes_table do 
       row :title
+      row :date
+      row :image do |row|
+        image_tag row.image.url(:cover)
+      end
     end  
    end
 end
