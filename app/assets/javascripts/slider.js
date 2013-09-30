993917b7ff4235cf182438d6611549396d7a5232
Viacheslav Bobrov <babrovka@gmail.com>
2013-09-30 18:04:31 +0400

    slider alpha

--- app/assets/javascripts/slider.js
+++ app/assets/javascripts/slider.js
@@ -1,7 +1,9 @@
 $(document).ready(function() {
-	
 
-
-
+	$("#left_arrow").on('click', function () {
+		$(".slide:nth-child(1)").hide();
+		$(".slide:nth-child(2)").show();
+		return false
+	});
 	
 });
