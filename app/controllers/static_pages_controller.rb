1309fcc3b5657deacf18e20b9978f14f6424c197
Viacheslav Bobrov <babrovka@gmail.com>
2013-10-12 17:09:12 +0400

    ordering

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,7 +2,7 @@ class StaticPagesController < ApplicationController
   def index
     @slider_hroniks = Hronik.published.order('created_at DESC').limit(5)
     hroniks_and_articles = (Article.published.all + Hronik.published.all).sort_by{|e| e[:date]}
-    sorted_olds = Old.published.all.sort_by{|e| e[:date]}
+    sorted_olds = Old.published.all.sort_by{|e| e[:created_at]}
     @elements = (hroniks_and_articles + sorted_olds).paginate(:page => params[:page], :per_page => 3)
   end
   
