f1637b79cb00dd6d75ea587ef7fa2b3b05d8ebc8
Viacheslav Bobrov <babrovka@gmail.com>
2013-11-20 15:36:16 +0400

    pdf fix

--- app/pdfs/issue_pdf.rb
+++ app/pdfs/issue_pdf.rb
@@ -2,15 +2,26 @@
 class IssuePdf < Prawn::Document
   include ActionView::Helpers::SanitizeHelper
 
+  def russian_font
+    font_families.update(
+      "serif" => {
+        :bold => "#{Rails.root}/app/assets/fonts/pt_serif_bold.ttf",
+        :normal  => "#{Rails.root}/app/assets/fonts/pt_serif_regular.ttf" },
+      "sans" => {
+        :bold => "#{Rails.root}/app/assets/fonts/pt_sans_bold.ttf",
+        :normal  => "#{Rails.root}/app/assets/fonts/pt_sans_regular.ttf" })
+  end
+
   def remove_html(string)
     sanitize(string, :tags => {}).gsub(/&quot;/i,"").gsub(/&nbsp;/i,"") # empty tags hash tells it to allow no tags
   end
   
   def initialize(issue, view)
+    russian_font
     super(top_margin: 30)
     @issue = issue
     @olds = issue.olds
-    russian_font
+    
     
     font "serif"
     text "#{@issue.title}", :align => :center, :size => 38, :style => :bold, :inline_format => true
@@ -30,14 +41,6 @@ class IssuePdf < Prawn::Document
     
   end
   
-  def russian_font
-    font_families.update(
-      "serif" => {
-        :bold => "#{Rails.root}/app/assets/fonts/pt_serif_bold.ttf",
-        :normal  => "#{Rails.root}/app/assets/fonts/pt_serif_regular.ttf" },
-      "sans" => {
-        :bold => "#{Rails.root}/app/assets/fonts/pt_sans_bold.ttf",
-        :normal  => "#{Rails.root}/app/assets/fonts/pt_sans_regular.ttf" })
-  end
+
   
 end
--- app/pdfs/old_pdf.rb
+++ app/pdfs/old_pdf.rb
@@ -1,15 +1,26 @@
 # coding: utf-8
 class OldPdf < Prawn::Document
   include ActionView::Helpers::SanitizeHelper
+  
+  def russian_font
+    font_families.update(
+      "serif" => {
+        :bold => "#{Rails.root}/app/assets/fonts/pt_serif_bold.ttf",
+        :normal  => "#{Rails.root}/app/assets/fonts/pt_serif_regular.ttf" },
+      "sans" => {
+        :bold => "#{Rails.root}/app/assets/fonts/pt_sans_bold.ttf",
+        :normal  => "#{Rails.root}/app/assets/fonts/pt_sans_regular.ttf" })
+  end
 
   def remove_html(string)
     sanitize(string, :tags => {}).gsub(/&quot;/i,"").gsub(/&nbsp;/i,"") # empty tags hash tells it to allow no tags
   end
   
   def initialize(old, view)
+    russian_font
     super(top_margin: 30)
     @old = old
-    russian_font
+    
     
     font "serif"
     text "#{@old.title}", :align => :center, :size => 38, :style => :bold, :inline_format => true
@@ -22,14 +33,6 @@ class OldPdf < Prawn::Document
     
   end
   
-  def russian_font
-    font_families.update(
-      "serif" => {
-        :bold => "#{Rails.root}/app/assets/fonts/pt_serif_bold.ttf",
-        :normal  => "#{Rails.root}/app/assets/fonts/pt_serif_regular.ttf" },
-      "sans" => {
-        :bold => "#{Rails.root}/app/assets/fonts/pt_sans_bold.ttf",
-        :normal  => "#{Rails.root}/app/assets/fonts/pt_sans_regular.ttf" })
-  end
+
   
 end
